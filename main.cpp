38f6c4c617ecbfe58b688ea2e60795935cbd9e3f
dgray32 <78519558+dgray32@users.noreply.github.com>
2021-03-04 14:00:19 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -11,15 +11,27 @@ int main()
    string fname, lname;
    int mm, dd, yy;
    int month, day, year;
-   cout << "Enter your name and date of birth (first name, last name, month, day, year): ";
-   cin >> fname >> lname >> mm >> dd >> yy;
-   cout << "Enter current date: ";
-   cin >> month >> day >> year;
+   cout << "Welcome to your target Heart Rate Calculator, Please provide your First name: ";
+   cin >> fname;
+   cout << "Last name: ";
+   cin >> lname;
+   cout << "Birth month: ";
+   cin >> mm;
+   cout << "Day of Birth ";
+   cin >> dd;
+   cout << "Birth Year: ";
+   cin >> yy;
+   cout << "Current month: ";
+   cin >> month;
+   cout << "Current day ";
+   cin >> day;
+   cout << "Current Year: ";
+   cin >> year;
    HeartRates person1( fname, lname, mm, dd, yy, month, day, year);
    cout << "Your name is " << person1.getFirstName() << " " << person1.getLastName() <<
        ". Your birthday is " << person1.getMonthOfBirth() << "/" << person1.getDayOfBirth() <<
        "/" << person1.getYearOfBirth() << endl;
    cout << "Your age is " << person1.getAge() << " years!" << endl;
-   cout << "Your maximum heart rate is " << person1.getMaximumHeartRate() << endl;
+   cout << "Your max heart rate is " << person1.getMaximumHeartRate() << endl;
    cout << "Your target heart rate is " << person1.getTargetHeartRate() << endl;
 }
